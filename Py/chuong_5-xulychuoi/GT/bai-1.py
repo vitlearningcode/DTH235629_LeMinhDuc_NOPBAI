f50376ee485a5f34145c0486c8b7facd59dbc8cb
DTH235629_LEMINHDUC <leduc071204@gmail.com>
2025-10-07 14:42:57 +0700

    gt1-5

---
+++ Py/chuong_5-xulychuoi/GT/bai-1.py
@@ -0,0 +1,10 @@
+'''Viết chương trình đổi các chữ đầu mỗi từ trong một câu thành chữ hoa.'''
+
+# Nhập chuỗi từ người dùng
+input_string = input("Nhập vào một câu: ")
+
+# Sử dụng phương thức title() để viết hoa chữ cái đầu mỗi từ
+capitalized_string = input_string.title()
+
+# In kết quả
+print("Chuỗi sau khi chuyển đổi:", capitalized_string)
